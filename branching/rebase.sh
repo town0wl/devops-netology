e2b31d0ca7975e48fe7a342cada2b4cb2983c0e7
Anna <jfdll@yandex.ru>
2021-05-20 23:41:46 +0300

    Rebase: git-rebase 2

--- branching/rebase.sh
+++ branching/rebase.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 # display command line options
 
-count=1
 for param in "$@"; do
-    echo "Parameter: $param"
-    count=$(( $count + 1 ))
+    echo "Next parameter: $param"
 done
 
 echo "====="
